f4ffe3c40779192e70e4d82d214aa869897b0da0
tcowern <tcowern@yahoo.com>
2016-11-15 14:30:19 -0700

    Tuesday Working 2

--- Phase1Refactor/controllers/buckets.js
+++ Phase1Refactor/controllers/buckets.js
@@ -48,17 +48,28 @@ function addBucket (req, res) {
     var user = req.session.userId;
     console.log("user: ", user);
     var bvar = req.body.buckets;
+    // var tasks = req.body.buckets[tasks];
+    // console.log("tasks", tasks);
         // 'description': req.body.description,
     
     // var newBarrel = new barrel(aryansvar);
     console.log("Edit Input haha: ", bvar);
-    Bucket.findOneAndUpdate({'_id': user}, {$push:{buckets:bvar}}, {new: true}, (err, doc) => {
+    Bucket.findOneAndUpdate({'_id': user}, {$push:{buckets:{"bucket" :bvar, "compb":"", "completed":false, "tasks":[{"name":"task 1", "comptd":"", "compt": false}, {"name":"task 2", "comptd":"", "compt": false}]}}}, {new: true}, (err, doc) => {
         if (err) {
             return res.send(err);
         }
-        console.log(doc);
+        console.log(doc.buckets[3].tasks);
         res.send(doc);
     });
+
+    // Bucket.findOneAndUpdate({'_id': user}, {$push:{buckets:{"bucket" :bvar, "compb":"", "completed":false, "tasks":[{"name":"task 1", "comptd":"", "compt": false}, {"name":"task 2", "comptd":"", "compt": false}]}}}, {new: true}, (err, doc) => {
+    //     if (err) {
+    //         return res.send(err);
+    //     }
+    //     console.log(doc.buckets[3].tasks);
+    //     res.send(doc);
+    // });
+
 
 }
 
